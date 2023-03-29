325b9f901b57f694c5b341129bc7c68b5d21ab99
bsmoon-86 <ekflafg@naver.com>
2023-03-29 18:04:10 +0900

    230328_update

--- vendingmachine/web/app.js
+++ vendingmachine/web/app.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
+const path =require('path')
 
 // 외부의 파일을 로드(같은 경로에 있는 client.js파일)
 const client = require("./client")
+// make_token.js 파일을 로드
+const token = require("./make_token")
 
 // express web 설정 
 // html 파일들을 어떤 경로에 있는 파일을 사용을 할지 지정
@@ -17,6 +20,10 @@ app.use(express.json())
 app.use(express.urlencoded({
     extended: false
 }))
+
+// 외부의 css, image, js 파일들을 저장해두는 공간의 위치를 지정
+// 일반적으로 public 폴더에 모아서 관리
+app.use(express.static(path.join(__dirname, 'public')))
 
 // api를 생성 
 // localhost:3000/ 생성
@@ -46,10 +53,93 @@ app.post("/input", async function(req, res){
 app.get("/sell", async (req, res)=>{
     // GET형식으로 보낸 데이터를 변수에 대입
     // GET형식은 POST와 달리 query 부분에 데이터가 존재
+    // 구입하려는 빵의 개수만큼 빵의 재고를 줄이고 
+    // 구입하는 사람의 빵의 재고를 늘린다.
+    // 빵의 구입을 하는 경우 생성한 토큰을 오너에게 보낸다.
+    // 조건 -> 빵을 구입할때 빵의 가격보다 지갑내의 토큰이 많거나 같은 경우에만 빵의 구입이 가능
+    //          토큰의 양이 부족하다면 구매는 실패
+    // 위의 조건이 만족하는 경우 
+    // 빵의 가격만큼 오너에게 토큰을 지불하고 빵을 받아가야된다
+    // 조건문은 if문으로 생성
+    // 조건 유저의 지갑의 토큰의 양 >= 빵의 가격
+    const _address = req.query.address
+    // 유저의 지갑에 남은 토큰의 양
+    // make_token.js에 있는 balance_of() 함수를 호출
+    // 이 함수는 매개변수 address를 가지고 있다
+    // 해당하는 주소의 토큰의 양을 출력하여 token_amount라는 변수에 대입
+    const token_amount = await token.balance_of(_address)
+    console.log(token_amount)
+    // 빵의 가격 10토큰 
+    // 구매하려는 빵의 토탈 금액 = 10 * data
     data = await parseInt(req.query.bread)
-    await client.bread_purchase(data)
-    console.log(data)
-    res.redirect("/")
+    const price = 10 * data
+    console.log(price)
+    // 조건문 시작 
+    if (token_amount >= price) {
+        // 오너에게 토큰을 보낸다
+        await token.trans_from_token(_address, price)
+        // 오너의 빵의 재고가 줄어드는 함수
+        await client.bread_purchase(data)
+        res.redirect("/")
+    }else{
+        res.send("소유하고 있는 토큰의 양이 빵의 가격보다 적습니다.")
+    }
+})
+
+// 토큰을 생성하는 api를 생성
+// localhost:3000/create_token 주소 생성
+app.get('/create_token', async (req, res)=>{
+    // make_token.js 파일 안에 있는 create_token이라는 함수를 호출 하여 리턴값을 token_address 변수에 대입
+    const token_address = await token.create_token()
+    // localhost:3000/create_token 주소로 요청했을때 token_address 웹 화면에 보여준다
+    res.send(token_address)
+})
+
+// 토큰을 거래하는 화면을 보여주는 api 생성
+// localhost:3000/transfer 주소 생성
+app.get("/transfer", (req, res)=>{
+    // views폴더 안에 있는 transfer.ejs 파일을 보여주겠다
+    res.render('transfer')
+})
+
+// 토큰을 거래하는 함수 생성 
+// localhost:3000/token_trans 라는 post형태의 주소 생성
+app.post("/token_trans", async (req, res)=>{
+    // 유저에게서 받아온 토큰의 양, 지갑 주소를 변수에 대입 
+    const _amount = parseInt(req.body.amount)
+    const _address = req.body.address
+    console.log(_amount, _address)
+    // make_token.js 파일 안에 있는 trans_token()함수를 호출
+    // 해당하는 함수에는 매개변수가 2개 존재
+    // address -> 토큰을 받을 지갑의 주소 ,  amount -> 보낼 토큰의 양
+    await token.trans_token(_address, _amount)
+
+    // 토큰의 거래가 완료되면 토큰 거래 화면으로 돌아간다
+    res.redirect('/transfer')
+    // transfer라는 함수는 토큰을 발생한 사람으로부터 유저의 지갑에 토큰을 보내는 함수
+    // 반대로 유저가 토큰을 발생한 사람에게 토큰을 보내는 방법은 되지 않는다.
+
+})
+
+// 유저가 오너에게 토큰을 보내는 함수 생성
+// localhost:3000/trans_owner 라는 POST 형태의 주소를 생성
+app.post('/trans_owner', async (req, res)=>{
+    // 유저가 보내온 데이터를 변수에 대입 
+    const _amount = parseInt(req.body.amount)
+    const _address = req.body.address
+    console.log(_amount, _address)
+
+    // make_token.js 안에 있는 trans_from_token() 함수를 호출
+    // address, amount 매개변수 
+    const receipt = await token.trans_from_token(_address, _amount)
+    console.log(receipt)
+
+    res.redirect("/transfer")
+})
+
+// log-in.ejs파일을 오픈하는 api 생성
+app.get("/login", (req, res)=>{
+    res.render('log-in')
 })
 
 // 서버를 시작
--- vendingmachine/web/make_token.js
+++ vendingmachine/web/make_token.js
@@ -12,9 +12,9 @@ caver.initKASAPI(chainId, accesskeyId, secretAccessKey)
 
 
 // 사용할 지갑의 주소를 등록
-const keyringContaner = new caver.keyringContaner()
-const keyring = keyringContaner.keyring.createFromPrivateKey('0xef0a0198393a7012063e4892fdeb4ead00a841beaa7c7188893833987d36d7c6')
-keyringContaner.add(keyring)
+const keyringContainer = new caver.keyringContainer()
+const keyring = keyringContainer.keyring.createFromPrivateKey('0xef0a0198393a7012063e4892fdeb4ead00a841beaa7c7188893833987d36d7c6')
+keyringContainer.add(keyring)
 
 // 토큰을 생성하는 함수( kip7 토큰 생성 )
 async function create_token(){
@@ -25,17 +25,18 @@ async function create_token(){
             decimals : 0, //토큰의 소수점 자리수
             initialSupply : 100000000 //토큰의 발행량
         }, 
-        keyring.addess, 
-        keyringContaner
+        keyring.address, 
+        keyringContainer
     )
     console.log(kip7._address)
+    return kip7._address
 }
 
 // 토큰을 거래하는 함수 
 async function trans_token(_address, _amount){
     // 발행한 토큰을 keyringContainer에 지정
-    const kip7 = await new caver.kct.kip7('발행된 토큰의 주소')
-    kip7.setWallet(keyringContaner)
+    const kip7 = await new caver.kct.kip7('0x44112F4b951BEf88d8b736cEbb0F9313a3bFb606')
+    kip7.setWallet(keyringContainer)
 
     // 발행한 토큰을 _address 매개변수로 받은 인자값의 주소로 _amount만큼 보내준다.
     const receipt = await kip7.transfer(_address, _amount, {from : keyring.address})
@@ -43,10 +44,45 @@ async function trans_token(_address, _amount){
     console.log(receipt)
 }
 
+// 유저가 토큰 발생자(owner)에게 토큰을 보낼수 있는 함수 
+async function trans_from_token(_address, _amount){
+    // 발행한 토큰을 keyringContainer에 지정
+    const kip7 = await new caver.kct.kip7('0x44112F4b951BEf88d8b736cEbb0F9313a3bFb606')
+    kip7.setWallet(keyringContainer)
+
+    // owner의 지갑 주소 
+    const owner = keyring.address
+    console.log(owner)
+
+
+    // 두번째 지갑의 주소를 KAS 환경에 등록
+    const keyring2 = keyringContainer.keyring.createFromPrivateKey('0x1b02d2bb3012f7218e90d336a935bd919d0741a5814252d51cf5c337984f2192') 
+    keyringContainer.add(keyring2)
+    // approve() 함수를 실행
+    // approve()는 오너의 주소가 아닌 유저의 지갑주소로 트랙젝션을 일으킨다. 
+    // approve('오너의 지갑주소', '사용할수 있는 토큰의 양', {from : 지갑의 주인의 주소})
+    await kip7.approve(owner, _amount, {from : keyring2.address})
+
+
+    // 일반적인 transfer는 받는 주소와 토큰 양만 지정이 가능
+    // transferFrom 함수는 토큰을 보내는 주소와 받는 주소 토큰의 양을 지정 가능
+    const receipt = await kip7.transferFrom(_address, owner, _amount, {from : keyring.address})
+
+    // 이 함수는 에러가 발생한다
+    // 에러가 발생하는 이유는 오너가 오너의 지갑이 아닌 다른 사람의 지갑의 토큰을 이동을 시킬수 없기때문
+    // 이 함수를 실행을 시키기 위해서는 approve 함수를 먼저 실행을 시켜야 한다. 
+    // approve()  함수가 하는 행동은 자기 자신의 지갑에서 일정 토큰의 양을 다른 사람이 이동 시킬수 있도록 허가하는 함수
+    // transferFrom() 함수를 사용하기 위해서는 approve()함수를 먼저 실행하여 토큰을 다른 사람이 사용할 수 있도록 허가를 해야한다. 
+
+
+    console.log(receipt)
+    
+}
+
 // 토큰의 양을 출력하는 함수 
 async function balance_of(_address){
-    const kip7 = await new caver.kct.kip7('발행된 토큰의 주소')
-    kip7.setWallet(keyringContaner)
+    const kip7 = await new caver.kct.kip7('0x44112F4b951BEf88d8b736cEbb0F9313a3bFb606')
+    kip7.setWallet(keyringContainer)
 
     // balanceOf 함수를 호출하여 해당하는 주소의 토큰의 양을 출력
     // 지갑의 토큰의 개수만 출력하는 함수 ( 데이터의 변화는 없다. )
@@ -66,5 +102,5 @@ async function create_wallet(){
 
 // 다른 파일에서 함수들을 호출하기 위해서 module.exports
 module.exports = {
-    create_token, create_wallet, trans_token, balance_of
+    create_token, create_wallet, trans_token, balance_of, trans_from_token
 }
